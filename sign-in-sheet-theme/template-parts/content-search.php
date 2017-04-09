5314dcbd340cdc2a1b940d302174c018b46e917b
johnk <johnk@riceball.com>
2017-04-09 05:14:59 -0700

    Category lists now look like searches.

--- sign-in-sheet-theme/template-parts/content-search.php
+++ sign-in-sheet-theme/template-parts/content-search.php
@@ -13,18 +13,10 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php _s_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
